f23adf42c866b098e1316c045a6615e807e54234
pauloJ-r <pj74586@gmail.com>
2023-11-22 01:27:10 -0300

    feat: criação da pagina sobre

---
+++ Projeto-recifArt/src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import "./Sobre.css"
+const Sobre = () => {
+    return (
+        <>
+        <div id="container-sobre">
+        <header className="header-sobre">
+          <Navbar />
+          </header>
+         </div>
+         <Footer />
+         
+        </>
+        )
+
+};
+export default Sobre;
