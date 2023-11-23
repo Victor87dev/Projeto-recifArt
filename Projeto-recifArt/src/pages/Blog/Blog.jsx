84c6b76e3222b8a68ab7b308d1d6c5f2f24f8e86
pauloJ-r <pj74586@gmail.com>
2023-11-23 19:12:36 -0300

    feat: criando branch pagina-blog

--- Projeto-recifArt/src/pages/Blog/Blog.jsx
+++ Projeto-recifArt/src/pages/Blog/Blog.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import "./Blog.css"
 
 const Blog = () => {
    return (
       <>
-      <h1>Blog</h1> 
+         <div id="container-blog">
+            <header className="header-blog">
+               <Navbar />
+                 <h1>Nosso Blog, Sua Jornada</h1>
+                 <p>Conectando, Inspirando e Educando para um Mundo Mais Sustentável e Criativo</p>
+            </header>
+            <main>
+            
+               
+            </main>
+
+            <Footer />
+
+         </div>
       </>
    )
-   
+
 }
 export default Blog;
