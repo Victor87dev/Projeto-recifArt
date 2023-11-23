5126fcd6a33a1202ee7eb30ccec9f657fcf9beb1
Victor87dev <devsjoaovvictor@gmail.com>
2023-11-23 20:29:11 -0300

    feat: banner page feirinha e componente ProdutoFeirinha

--- Projeto-recifArt/src/pages/Feirinha/Feirinha.jsx
+++ Projeto-recifArt/src/pages/Feirinha/Feirinha.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import "./Feirinha.css";
 
 const Feirinha = () => {
-   return (
-      <>
-      <h1>Feirinha</h1> 
-      </>
-   )
-   
-}
+  return (
+    <>
+      <div id="container-feirinha">
+        <header className="header-feirinha">
+          <Navbar />
+          <h1>Conheça nossa Feirinha</h1>
+        </header>
+        <section className="sessao-feirinha">
+          <div className="box-input">
+            <input type="text" className="input-pesquisar" placeholder="Pesquisar..."/>
+          </div>
+
+          <div className="produtos-grid">
+            
+          </div>
+        </section>
+        
+        <Footer />
+      </div>
+    </>
+  );
+};
 export default Feirinha;
