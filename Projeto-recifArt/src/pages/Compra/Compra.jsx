b06496abd3beafc061adcfcb0c2bae9271eb4d1c
Victor87dev <devsjoaovvictor@gmail.com>
2023-11-24 18:07:35 -0300

    feat: adicionando navbar e footer

--- Projeto-recifArt/src/pages/Compra/Compra.jsx
+++ Projeto-recifArt/src/pages/Compra/Compra.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 import "./Compra.css";
 
 const Compra = () => {
- 
   return (
     <>
-      <h1>Compra</h1>
+      <div id="container-compra">
+        <header className="header-compra">
+          <Navbar />
+          <h1>Finalizando Compra</h1>
+        </header>
+        <Footer />
+      </div>
     </>
   );
 };
