430f88031e13f9a1edc0ecfd0d0c443fc74404fe
Victor87dev <devsjoaovvictor@gmail.com>
2023-11-25 01:47:29 -0300

    feat: pagina de compra finalizada

--- Projeto-recifArt/src/components/CardCompra/CardCompra.jsx
+++ Projeto-recifArt/src/components/CardCompra/CardCompra.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./CardCompra.css";
 
-const CardCompra = () => {
- 
+const CardCompra = (props) => {
   return (
     <>
-      <h2>Comprar</h2>
+      <img src={props.img} alt="produto" className="img-urso"/>
+      <div className="box-informacoes">
+        <h2>{props.titulo}</h2>
+        <ol>
+          <li><span>●</span><span className="azul">Material:</span> {props.material}</li>
+          <li><span>●</span><span className="azul">Tamanho:</span> {props.tamanho}</li>
+          <li><span>●</span><span className="azul">Cores Disponíveis:</span> {props.cores}</li>
+        </ol>
+        <input type="number" placeholder="Selecione a quantidade" />
+        <p className="p-preco">{props.preco}</p>
+        <button type="button">Comprar</button>
+        <img src={props.perfil} alt="" className="img-perfil"/>
+        <p className="p-nome">{props.nome}</p>
+      </div>
     </>
   );
 };
